8aa58a14ea09e0456ed0417fcf04bcc952f2b5b2
Farrel Franqois <farrel@franqois.id>
2019-05-31 04:29:26 +0700

    Merubah subdomain pada CDN.

--- functions.php
+++ functions.php
@@ -22,18 +22,18 @@ add_filter( 'get_search_form', function() { return null;} );
 
 function image_cdn_sitemap_fix($uri)
 {
-    return str_replace( 'https://s.wpinter.com', 'https://cdn.staticaly.com/img/farrelf.wpinter.com', $uri );
+    return str_replace( 'https://s.wpinter.com', 'https://cdn.statically.io/img/farrelf.wpinter.com', $uri );
 }
 add_filter('wpseo_xml_sitemap_img_src', 'image_cdn_sitemap_fix');
 
 function emoji_svg_cdn_url()
 {
-  return $default_url = "https://cdn.staticaly.com/gh/twitter/twemoji/v11.2.0/2/svg/";
+  return $default_url = "https://cdn.statically.io/libs/twemoji/12.0.4/2/svg/";
 }
 add_filter('emoji_svg_url', 'emoji_svg_cdn_url');
 
 function emoji_png_cdn_url()
 {
-  return $default_url = "https://cdn.staticaly.com/gh/twitter/twemoji/v11.2.0/2/72x72/";
+  return $default_url = "https://cdn.statically.io/libs/twemoji/12.0.4/2/72x72/";
 }
 add_filter('emoji_url', 'emoji_png_cdn_url');
